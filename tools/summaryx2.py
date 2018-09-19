784884da08bf0a6595d05bec128b5f575129ac85
Todd Brandt <todd.e.brandt@linux.intel.com>
2018-09-19 15:42:59 -0700

    tools/summaryx2: - add --issues option to parse dmesg logs for warnings, errors, etc - fully integrated issues data into both html and text output - add --urlprefix option to use in links to timelines in the issue data

--- tools/summaryx2.py
+++ tools/summaryx2.py
@@ -28,7 +28,47 @@ import smtplib
 sys.path += ['..', '.']
 import sleepgraph as sg
 
-def info(file, data):
+def dmesg_issues(file, errinfo):
+	errlist = sg.Data.errlist
+	lf = sg.sysvals.openlog(file, 'r')
+	i = 0
+	list = []
+	for line in lf:
+		i += 1
+		m = re.match('[ \t]*(\[ *)(?P<ktime>[0-9\.]*)(\]) (?P<msg>.*)', line)
+		if not m:
+			continue
+		t = float(m.group('ktime'))
+		msg = m.group('msg')
+		for err in errlist:
+			if re.match(errlist[err], msg):
+				if err not in errinfo:
+					errinfo[err] = []
+				found = False
+				for entry in errinfo[err]:
+					if re.match(entry['match'], msg):
+						entry['count'] += 1
+						found = True
+						break
+				if found:
+					continue
+				arr = msg.split()
+				for j in range(len(arr)):
+					if re.match('^[0-9\-\.]*$', arr[j]):
+						arr[j] = '[0-9\-\.]*'
+					else:
+						arr[j] = arr[j].replace(']', '\]').replace('[', '\[').replace('.', '\.').replace('+', '\+')
+				mstr = ' '.join(arr)
+				entry = {
+					'line': msg,
+					'match': mstr,
+					'count': 1,
+					'url': os.path.abspath(file)
+				}
+				errinfo[err].append(entry)
+				break
+
+def info(file, data, errcheck):
 	html = open(file, 'r').read()
 	line = sg.find_in_html(html, '<div class="stamp">', '</div>')
 	x = re.match('^(?P<host>.*) (?P<kernel>.*) (?P<mode>.*) \((?P<info>.*)\)', line)
@@ -36,6 +76,7 @@ def info(file, data):
 		print 'WARNING: unrecognized formatting in summary file' % file
 		return
 	h, k, m, r = x.groups()
+	errinfo = dict()
 	res = []
 	total = -1
 	for i in re.findall(r"[\w ]+", r):
@@ -64,6 +105,7 @@ def info(file, data):
 	for test in html.split('<tr'):
 		if '<th>' in test or 'class="head"' in test or '<html>' in test:
 			continue
+		dmesg = ''
 		values = []
 		out = test.split('<td')
 		for i in out[1:]:
@@ -76,12 +118,27 @@ def info(file, data):
 			if values[11] not in wres:
 				wres[values[11]] = 0
 			wres[values[11]] += 1
+		if not errcheck:
+			continue
+		if values[13]:
+			x = re.match('<a href="(?P<u>.*)">', values[13])
+			dcheck = file.replace('summary.html', x.group('u').replace('.html', '_dmesg.txt.gz'))
+			if os.path.exists(dcheck):
+				dmesg = dcheck
+			elif os.path.exists(dmesg[:-3]):
+				dmesg = dcheck[:-3]
+		if values[6] and values[6] != 'NETLOST' and dmesg:
+			dmesg_issues(dmesg, errinfo)
 	wstext = dict()
 	for i in sorted(wsus, key=lambda k:wsus[k], reverse=True):
 		wstext[wsus[i]] = i
 	wrtext = dict()
 	for i in sorted(wres, key=lambda k:wres[k], reverse=True):
 		wrtext[wres[i]] = i
+	issues = dict()
+	for err in errinfo:
+		for entry in errinfo[err]:
+			issues[entry['count']] = entry
 	data[k][h][m] = {
 		'file': file,
 		'results': res,
@@ -89,6 +146,7 @@ def info(file, data):
 		'rstat': [rmax, rmed, rmin],
 		'wsd': wstext,
 		'wrd': wrtext,
+		'issues': issues
 	}
 
 def text_output(data):
@@ -112,12 +170,26 @@ def text_output(data):
 				text += '   Worst Resume Devices:\n'
 				for cnt in sorted(info['wrd'], reverse=True):
 					text += '   - %s (%d times)\n' % (info['wrd'][cnt], cnt)
+				issues = info['issues']
+				if len(issues) < 1:
+					continue
+				text += '   Issues found in dmesg logs:\n'
+				for e in sorted(issues, reverse=True):
+					text += '   (x%d) %s\n' % (e, issues[e]['line'])
 	return text
 
-def html_output(data):
+def get_url(dmesgfile, urlprefix):
+	html = dmesgfile.replace('.gz', '').replace('_dmesg.txt', '.html')
+	idx = html.find('pm-graph-test')
+	if not urlprefix or idx < 0:
+		return '<a href="file://%s">html</a>' % html
+	idx += len('pm-graph-test')
+	return '<a href="%s">html</a>' % (urlprefix+html[idx:])
+
+def html_output(data, urlprefix, showerrs):
 	html = '<!DOCTYPE html>\n<html>\n<head>\n\
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">\n\
-		<title>SleepGraph Summary</title>\n\
+		<title>SleepGraph Summary of Summaries</title>\n\
 		<style type=\'text/css\'>\n\
 			table {width:100%; border-collapse: collapse;}\n\
 			.summary {border:0px solid;}\n\
@@ -142,15 +214,11 @@ def html_output(data):
 			th.format('Worst Resume Devices') + '</tr>\n'
 		num = 0
 		for host in sorted(data[kernel]):
-			first = True
 			for mode in sorted(data[kernel][host], reverse=True):
-				html += '<tr class=alt>\n' if num % 2 == 1 else '<tr>\n'
+				trs = '<tr class=alt>\n' if num % 2 == 1 else '<tr>\n'
+				html += trs
 				info = data[kernel][host][mode]
-				if first:
-					html += tdo.format(host, ' align=center')
-					first = False
-				else:
-					html += '<td></td>'
+				html += tdo.format(host, ' align=center')
 				html += td.format(mode)
 				tdhtml = '<table>'
 				for val in info['results']:
@@ -167,6 +235,18 @@ def html_output(data):
 						tdhtml += '<li>%s (x%d)</li>' % (info[entry][cnt], cnt)
 					html += tdo.format(tdhtml+'</ul>', ' class=devlist')
 				html += '</tr>\n'
+				if not showerrs:
+					continue
+				html += '%s<td colspan=7><table border=1>' % trs
+				html += '%s<td colspan=5><b>Issues found</b></td><td><b>Count</b></td><td><b>html</b></td>\n</tr>' % trs
+				issues = info['issues']
+				if len(issues) > 0:
+					for e in sorted(issues, reverse=True):
+						html += '%s<td colspan=5>%s</td><td>%d times</td><td>%s</td>\n<tr>\n' % \
+							(trs, issues[e]['line'], e, get_url(issues[e]['url'], urlprefix))
+				else:
+					html += '%s<td colspan=7>NONE</td>\n<tr>\n' % trs
+				html += '</table></td></tr>'
 			num += 1
 		html += '</table>\n'
 	html += '</body>\n</html>\n'
@@ -179,9 +259,10 @@ def send_mail(server, sender, receiver, type, subject, contents):
 		'MIME-Version: 1.0\n'\
 		'Content-type: %s\n'\
 		'Subject: %s\n\n' % (sender, receiver, type, subject)
+	receivers = receiver.split(';')
 	message += contents
 	smtpObj = smtplib.SMTP(server, 25)
-	smtpObj.sendmail(sender, receiver, message)
+	smtpObj.sendmail(sender, receivers, message)
 
 def doError(msg, help=False):
 	print("ERROR: %s") % msg
@@ -193,24 +274,34 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Generate a summary of a summaries')
 	parser.add_argument('--html', action='store_true',
 		help='output in html (default is text)')
+	parser.add_argument('--issues', action='store_true',
+		help='extract issues from dmesg files (WARNING/ERROR etc)')
 	parser.add_argument('--mail', nargs=3, metavar=('server', 'sender', 'receiver'),
 		help='send the output via email')
 	parser.add_argument('--subject', metavar='string',
 		help='the subject line for the email')
+	parser.add_argument('--urlprefix', metavar='url',
+		help='url prefix to use in links to timelines')
 	parser.add_argument('folder', help='folder to search for summaries')
 	args = parser.parse_args()
 
 	if not os.path.exists(args.folder) or not os.path.isdir(args.folder):
 		doError('Folder not found')
 
+	if args.urlprefix:
+		if args.urlprefix[-1] == '/':
+			args.urlprefix = args.urlprefix[:-1]
+		if not args.urlprefix.endswith('pm-graph-test') :
+			doError('urlprefix must end with pm-graph-test')
+
 	data = dict()
 	for dirname, dirnames, filenames in os.walk(args.folder):
 		for filename in filenames:
 			if filename == 'summary.html':
 				file = os.path.join(dirname, filename)
-				info(file, data)
+				info(file, data, args.issues)
 
-	out = html_output(data) if args.html else text_output(data)
+	out = html_output(data, args.urlprefix, args.issues) if args.html else text_output(data)
 
 	if args.mail:
 		server, sender, receiver = args.mail
