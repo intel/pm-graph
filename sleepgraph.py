b2e89fa859864166756dde76fa939cf8031f4d05
Todd Brandt <todd.e.brandt@linux.intel.com>
2019-03-07 09:43:22 -0800

    googlesheet: - merge the worst suspend/resume device columns into one - add more descriptive column headers to summary - add comments to all summary tab items - add occurance rate to kernel issues tab sleepgraph: - run processData even if an error has occurred

--- sleepgraph.py
+++ sleepgraph.py
@@ -6093,7 +6093,7 @@ def runTest(n=0):
 	if sysvals.skiphtml:
 		sysvals.sudoUserchown(sysvals.testdir)
 		return
-	if len(testdata) > 0 and not testdata[0]['error']:
+	if len(testdata) > 0:
 		testruns, stamp = processData(True)
 		for data in testruns:
 			del data
