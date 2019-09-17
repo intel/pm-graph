72eb16f4427730f2782c8dde21383ce384ebbd57
Todd Brandt <todd.e.brandt@linux.intel.com>
2019-09-17 05:55:52 -0700

    tools/dataserver: - handle import of parallel module in python3 - include an ssh key test on start, fail if not successful - walk user through the ssh key setup process

--- tools/dataserver.py
+++ tools/dataserver.py
@@ -5,7 +5,10 @@ import sys
 import time
 from subprocess import call, Popen, PIPE
 from datetime import date, datetime, timedelta
-import parallel
+try:
+	from tools.parallel import AsyncProcess
+except:
+	pass
 
 class DataServer:
 	ip = 'otcpl-perf-data.jf.intel.com'
@@ -14,8 +17,32 @@ class DataServer:
 			disk = (datetime.now().second % 8) + 1
 		self.user = user
 		self.rpath = '/media/disk%d/pm-graph-test' % disk
+	def sshcopyid(self):
+		res = call('ssh-copy-id %s@%s' % (self.user, self.ip), shell=True)
+		return res == 0
+	def sshkeyworks(self):
+		cmd = 'ssh -q -o BatchMode=yes -o ConnectTimeout=5 %s@%s echo -n' % \
+			(self.user, self.ip)
+		res = call(cmd, shell=True)
+		return res == 0
+	def setup(self):
+		if self.sshkeyworks():
+			return True
+		print('You must have an account on %s and an authorized ssh key\nto use this tool. '\
+			'I will try to add your id_rsa.pub using ssh-copy-id...' %\
+			(self.ip))
+		if not self.sshcopyid():
+			return False
+		if not self.sshkeyworks():
+			print('ERROR: failed to setup ssh key access.')
+			return False
+		print('SUCCESS: you now have password-less access.\n')
+		return True
+	def setupordie(self):
+		if not self.setup():
+			sys.exit(1)
 	def sshproc(self, cmd, timeout=60):
-		return parallel.AsyncProcess(('ssh %s@%s "{0}"' % (self.user, self.ip)).format(cmd), timeout, self.ip)
+		return AsyncProcess(('ssh %s@%s "{0}"' % (self.user, self.ip)).format(cmd), timeout, self.ip)
 	def sshcmd(self, cmd, timeout=60):
 		ap = self.sshproc(cmd, timeout)
 		out = ap.runcmd()
@@ -25,8 +52,6 @@ class DataServer:
 		return out
 	def scpfile(self, file, dir):
 		call('scp %s %s@%s:%s/' % (file, self.user, self.ip, dir), shell=True)
-	def enablessh(self):
-		call('ssh-copy-id %s@%s' % (self.user, self.ip), shell=True)
 	def uploadfolder(self, folder):
 		if not os.path.exists(folder):
 			print('ERROR: %s does not exist' % folder)
@@ -57,25 +82,25 @@ class DataServer:
 # exec start (skipped if script is loaded as library)
 if __name__ == '__main__':
 	import argparse
+	from parallel import AsyncProcess
+
 	user = '' if 'USER' not in os.environ else os.environ['USER']
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-nosshkey', action='store_true',
-		help='Skip adding your id_rsa.pub key to the remote server')
-	parser.add_argument('-user', metavar='name', default=user,
+	parser.add_argument('-u', '-user', metavar='name', default=user,
 		help='username for data server')
-	parser.add_argument('-u', '-upload', metavar='folder',
+	parser.add_argument('-upload', metavar='folder',
 		help='upload a sleepgraph multitest folder to otcpl-perf-data')
 	args = parser.parse_args()
 
-	if not args.user:
+	if not args.u:
 		print('ERROR: a username is required, please set $USER or use -user')
 		sys.exit(1)
 
-	ds = DataServer(args.user)
+	ds = DataServer(args.u)
+	ds.setupordie()
 
-	if not args.nosshkey:
-		ds.enablessh()
-
-	if args.u:
-		ds.uploadfolder(args.u)
+	if args.upload:
+		ds.uploadfolder(args.upload)
+	else:
+		print(ds.sshcmd('df /media/disk*'))
