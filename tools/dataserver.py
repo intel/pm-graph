c1e1af9026e1b46d3226dd935667e97df906ecaf
Todd Brandt <todd.e.brandt@linux.intel.com>
2019-10-21 12:31:11 -0700

    tools/dataserver: - allow pre-existing tarballs - allow up to 3 concurrent googlesheet processses on the data server - do a formal check for tar.gz intergrity googlesheet: - do a formal check for tar.gz intergrity

--- tools/dataserver.py
+++ tools/dataserver.py
@@ -49,50 +49,56 @@ class DataServer:
 	def scpfile(self, file, dir):
 		res = call('scp %s %s@%s:%s/' % (file, self.user, self.host, dir), shell=True)
 		return res == 0
-	def uploadfolder(self, folder, monitor):
-		if not os.path.exists(folder):
-			print('ERROR: %s does not exist' % folder)
-			self.die()
-		if not os.path.isdir(folder):
-			print('%s is not a folder' % folder)
-			self.die()
+	def tarfolder(self, folder):
 		pdir, tdir = os.path.dirname(folder), os.path.basename(folder)
 		pdir = pdir if pdir else '.'
 		tarball = '/tmp/%s.tar.gz' % tdir
-		logfile = 'multitest-%s-%s.log' % (datetime.now().strftime('%y%m%d-%H%M%S'), tdir)
 		print('Taring up %s for transport...' % folder)
 		res = call('cd %s; tar cvzf %s %s > /dev/null' % (pdir, tarball, tdir), shell=True)
 		if res != 0:
 			print('ERROR: failed to create the tarball')
 			self.die()
+		return tarball
+	def uploadfolder(self, folder, monitor):
+		if not os.path.exists(folder):
+			print('ERROR: %s does not exist' % folder)
+			self.die()
+		tarball = self.tarfolder(folder) if os.path.isdir(folder) else folder
 		print('Sending tarball to server...')
 		if not self.scpfile(tarball, '/tmp'):
 			print('ERROR: could not upload the tarball')
-			os.remove(tarball)
 			self.die()
+		rtarball = '/tmp/' + os.path.basename(tarball)
 		if monitor:
 			print('Processing the data on the server...')
 			res = call('ssh %s@%s "multitest %s"' % \
-				(self.user, self.host, tarball), shell=True)
+				(self.user, self.host, rtarball), shell=True)
 		else:
 			print('Notifying server of new data...')
+			logfile = 'multitest-%s.log' % (datetime.now().strftime('%y%m%d-%H%M%S'))
 			res = call('ssh -n -f %s@%s "multitest %s > %s 2>&1 &"' % \
-				(self.user, self.host, tarball, logfile), shell=True)
+				(self.user, self.host, rtarball, logfile), shell=True)
 			print('Logging at %s' % logfile)
 		if res != 0:
 			print('ERROR: server processing failed')
-			os.remove(tarball)
 			self.die()
-		os.remove(tarball)
+		if os.path.isdir(folder):
+			os.remove(tarball)
 		print('Upload Complete')
 	def openshell(self):
 		call('ssh -X %s@%s' % (self.user, self.host), shell=True)
-	def by_my_lonesome(self):
+	def hasbandwidth(self):
 		out = self.sshcmd('ps aux')
+		count = 0
 		for line in out.split('\n'):
 			if 'googlesheet' in line and '-webdir' in line:
-				return False
+				count += 1
+		if count > 2:
+			return False
 		return True
+	def istarball(self, file):
+		res = call('tar -tzf %s > /dev/null 2>&1' % file, shell=True)
+		return res == 0
 	def die(self):
 		sys.exit(1)
 
@@ -109,13 +115,17 @@ if __name__ == '__main__':
 	parser.add_argument('folder',
 		help='multitest folder, or "shell" to open an ssh shell')
 	args = parser.parse_args()
+	usetarball = False
 
-	if args.folder != 'shell' and \
-		(not os.path.exists(args.folder) or not os.path.isdir(args.folder)):
-		print('ERROR: %s is not a valid folder' % args.folder)
+	if args.folder != 'shell' and not os.path.exists(args.folder):
+		print('ERROR: %s is not a valid file or folder' % args.folder)
 		sys.exit(1)
 
 	ds = DataServer('sleepgraph', 'otcpl-perf-data.jf.intel.com')
+
+	if not os.path.isdir(args.folder) and not ds.istarball(args.folder):
+		print('ERROR: %s is not a valid tarball (tar.gz)' % args.folder)
+		sys.exit(1)
 
 	if args.sshkeysetup:
 		ds.setupordie()
@@ -124,7 +134,7 @@ if __name__ == '__main__':
 		ds.openshell()
 		sys.exit()
 
-	if not ds.by_my_lonesome():
+	if not ds.hasbandwidth():
 		print('Server is currently processing other data, please try again later.')
 		print('Exitting...')
 		sys.exit(1)
