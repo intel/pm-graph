e232b59c21d710f6b12f7da69030f8351adcbf1e
Todd Brandt <todd.e.brandt@linux.intel.com>
2020-10-30 10:46:03 -0700

    sleepgraph: fix typo

--- sleepgraph.py
+++ sleepgraph.py
@@ -6042,7 +6042,7 @@ def rerunTest(htmlfile=''):
 def runTest(n=0, quiet=False):
 	# prepare for the test
 	sysvals.initTestOutput('suspend')
-	op = sysvals.writeDatafileHeader(self.dmesgfile, [])
+	op = sysvals.writeDatafileHeader(sysvals.dmesgfile, [])
 	op.write('# EXECUTION TRACE START\n')
 	op.close()
 	if n <= 1:
