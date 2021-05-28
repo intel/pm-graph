5824b501d0e79a5b680e2e64f632064da3cdae84
Todd Brandt <todd.e.brandt@linux.intel.com>
2021-05-28 12:45:36 -0700

    intel-updatecache: add new commands

--- tools/intel-updatecache.sh
+++ tools/intel-updatecache.sh
@@ -6,6 +6,31 @@ WEBDIR="$HOME/pm-graph-test"
 SORTDIR="$HOME/pm-graph-sort"
 MS="$HOME/.machswap"
 GS="python3 $HOME/pm-graph/stressreport.py"
+CACHE="$HOME/.multitests"
+DATACACHE="$HOME/.multitestdata"
 
-cd $WEBDIR
-$GS -webdir $WEBDIR -sortdir $SORTDIR -machswap $MS -urlprefix $URL -parallel 8 -sort test .
+printUsage() {
+	echo "USAGE: intel-updatecache command"
+	echo "COMMANDS:"
+	echo "   showmissing - show the multitests that have no data"
+	echo "   all - update both the multitest and multitestdata caches"
+	echo "   data - update only the multitestdata cache"
+	exit 0
+}
+
+if [ $# -gt 2 -o $# -lt 1 ]; then printUsage; fi
+
+if [ $1 = "help" ]; then
+	printUsage
+elif [ $1 = "showmissing" ]; then
+	cat $DATACACHE | sed "s/|.*//g" | sort > /tmp/check2.txt
+	cat $CACHE | sort > /tmp/check1.txt
+	diff /tmp/check1.txt /tmp/check2.txt
+	rm /tmp/check1.txt /tmp/check2.txt
+elif [ $1 = "all" ]; then
+	cd $WEBDIR
+	$GS -webdir $WEBDIR -sortdir $SORTDIR -machswap $MS -urlprefix $URL -parallel 8 -sort test .
+elif [ $1 = "data" ]; then
+	cd $WEBDIR
+	$GS -cache -webdir $WEBDIR -sortdir $SORTDIR -machswap $MS -urlprefix $URL -parallel 8 -sort test .
+fi
