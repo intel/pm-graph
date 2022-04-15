fbbb38f22982ed3170ae6f4b3ec41900ebb8ea48
Todd Brandt <todd.e.brandt@intel.com>
2022-04-15 11:52:10 -0700

    intel-stresstest-friday: add build of turbostat

--- tools/intel-stresstest-friday.sh
+++ tools/intel-stresstest-friday.sh
@@ -11,6 +11,7 @@ LOG=`date "+/home/sleepgraph/stresslogs/%y%m%d-%H%M%S-stresstest.txt"`
 
 labmachine killjobs > $LOG 2>&1
 intel-stresstest reset >> $LOG 2>&1
+intel-stresstest turbostat >> $LOG 2>&1
 intel-stresstest online restart >> $LOG 2>&1
 intel-stresstest install >> $LOG 2>&1
 sleep 120
