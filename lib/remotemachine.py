5113424db95cb7877493c2fa77dd4a5619a7e1a5
Todd Brandt <todd.e.brandt@linux.intel.com>
2022-06-15 17:54:51 -0700

    lib/remotemachine: remove handling of home folder netfix config

--- lib/remotemachine.py
+++ lib/remotemachine.py
@@ -255,8 +255,6 @@ class RemoteMachine:
 			git = 'http_proxy=%s %s' % (proxy, git)
 		cmd = 'cd /tmp ; rm -rf pm-graph ; ' + git + \
 			' ; cd pm-graph ; sudo make uninstall ; sudo make install'
-		self.sshcmd(cmd, 100)
-		cmd = 'test -f ~/netfix.cfg && sudo cp -f ~/netfix.cfg /usr/lib/pm-graph/config/netfix.cfg'
 		return self.sshcmd(cmd, 100)
 	def list_kernels(self, fatal=False):
 		versions = []
