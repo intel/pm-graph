170dd390e4752609750e0b1cb5a0f34ef1d4d216
Todd Brandt <todd.e.brandt@linux.intel.com>
2022-07-01 12:24:42 -0700

    netfix: handle the case where iwconfig is not found, only used in status

--- tools/netfix.py
+++ tools/netfix.py
@@ -351,12 +351,12 @@ class Wifi(NetDev):
 			out = fp.read().decode('ascii', 'ignore').strip()
 			fp.close()
 		except:
-			return ''
+			return 'UNKNOWN'
 		for line in out.split('\n'):
 			m = re.match('\s*(?P<dev>\S*)\s*.*ESSID:"(?P<net>\S*)".*', line)
 			if m and (m.group('dev') == self.dev):
 				return m.group('net')
-		return ''
+		return 'INACTIVE'
 	def driver_on(self):
 		if not self.drv:
 			return False
@@ -451,9 +451,7 @@ class Wifi(NetDev):
 		if self.drv:
 			stat = 'ACTIVE' if self.activeDriver() else 'INACTIVE'
 			self.printLine('Driver  "%s"' % self.drv, stat)
-		ssid = self.activeSSID()
-		name = ssid if ssid else 'INACTIVE'
-		self.printLine('WIFI AP "%s"' % self.dev, name)
+		self.printLine('WIFI AP "%s"' % self.dev, self.activeSSID())
 		self.anet = self.activeNetwork()
 		if self.net and self.anet and self.net != self.anet:
 			self.printLine('Network "%s"' % self.dev,
