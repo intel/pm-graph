e56e580e2f93ba813fd339ab23381522dc578e4a
Todd Brandt <todd.e.brandt@intel.com>
2022-11-02 16:00:59 -0700

    intel-stresstest-sunday: some machines only work on wifi

--- tools/intel-stresstest-sunday.sh
+++ tools/intel-stresstest-sunday.sh
@@ -18,7 +18,7 @@ fi
 LOG=`date "+/home/sleepgraph/stresslogs/%y%m%d-%H%M%S-stresstest.txt"`
 
 labmachine killjobs > $LOG 2>&1
-intel-stresstest resetwired >> $LOG 2>&1
+intel-stresstest reset >> $LOG 2>&1
 intel-stresstest turbostat >> $LOG 2>&1
 intel-stresstest online restart >> $LOG 2>&1
 intel-stresstest install >> $LOG 2>&1
