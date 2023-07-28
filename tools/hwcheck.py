5067749530c501376ffb77cd3a9d02c1795b034d
Todd Brandt <todd.e.brandt@intel.com>
2023-07-28 12:43:16 -0700

    hwcheck: fix -show all

--- tools/hwcheck.py
+++ tools/hwcheck.py
@@ -262,7 +262,7 @@ if __name__ == '__main__':
 					print('%-24s: %s' % (name, out[name]))
 			else:
 				call('%s 2>/dev/null' % (datalist[t]['cmd']), shell=True)
-			sys.exit(0)
+		sys.exit(0)
 
 	log = LogFile()
 	log.runCheck(args.command)
